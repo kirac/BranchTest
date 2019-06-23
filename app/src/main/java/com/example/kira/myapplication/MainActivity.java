f15f4cd726a4b72821228d19e1176fd375ad65f1
kirac <576935777@qq.com>
2019-06-23 11:46:53 +0800

    Master push

--- app/src/main/java/com/example/kira/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kira/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //master分支
-        //master
+        //master push
     }
 }
